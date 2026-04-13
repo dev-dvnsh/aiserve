8cd25ce4c7275d6e0f236793bd1264290cf3bbf2
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2026-04-13 21:56:08 +0530

    cli tool written

---
+++ bin/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import inquirer from "inquirer";
+import fs from "fs";
+import { startServer } from "../server/server.js";
+
+const program = new Command();
+
+program
+  .command("serve [file]")
+  .description("Serve file as AI generated webpage")
+  .action(async (file) => {
+    let filePath = file;
+
+    if (!filePath) {
+      const answer = await inquirer.prompt([
+        {
+          type: "input",
+          name: "file",
+          message: "Enter file path:",
+        },
+      ]);
+
+      filePath = answer.file;
+    }
+
+    if (!fs.existsSync(filePath)) {
+      console.log("File not found");
+      process.exit(1);
+    }
+
+    startServer(filePath);
+  });
+
+program.parse(process.argv);
